d77e7c1a203764e115239afb87c1c00d75f5f1fa
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2026-01-28 16:26:06 +0530

    completed the product component

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
 
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1 className="fs-2 mt-5">The TradeGuru Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted mt-3">Thematic investing platform that helps you invest in diversified baskets of stocks on ETFs.</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/streakLogo.png" style={{height:"45px"}}/>
+          <p className="text-small text-muted mt-3">Systematic trading platform that allows you to create and backtest strategies without coding.</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/sensibullLogo.svg" style={{height:"45px"}}/>
+          <p className="text-small text-muted mt-3">Options trading platform that lets you create strategies, analyze positions, and examine data points like open interest, FII/DII, and more.</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/zerodhaFundhouse.png"style={{height:"45px"}}/>
+          <p className="text-small text-muted mt-3">Our asset management venture that is creating simple and transparent index funds to help you save for your goals.</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/goldenpiLogo.png" style={{height:"45px"}}/>
+          <p className="text-small text-muted mt-3">We also provide bond trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/dittoLogo.png" style={{height:"45px"}}/>
+          <p className="text-small text-muted mt-3">Personalized advice on life and health insurance. No spam and no mis-selling. </p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
