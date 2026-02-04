d144925c3d31b622390764cc661393f921f48015
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2026-02-04 23:46:00 +0530

    setup the basic backend server

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+const mongoose = require("mongoose");
+
+
+
+app.listen(port, () => {
+    console.log(`Server is on port ${port}`);
+    mongoose.connect(uri);  
+    console.log(`DataBase Connected`);
+});
