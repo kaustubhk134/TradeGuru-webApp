cb86fd71020572eb81d36f375751f846d273e884
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2026-02-06 01:26:54 +0530

    added the stock buying functionallity

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 
 const {HoldingsModel} = require("./model/HoldingsModel");
 const {PositionsModel} = require("./model/PositionsModel");
+const {OrdersModel} = require("./model/OrdersModel");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -189,6 +190,18 @@ app.get("/allPositions", async(req, res) => {
     let allPositions = await PositionsModel.find({});
     res.json(allPositions);
 });
+
+app.post("/newOrder", async(req, res) => {
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+
+    newOrder.save();
+    res.send("Order Saved");
+})
 
 app.listen(port, () => {
     console.log(`Server is on port ${port}`);
